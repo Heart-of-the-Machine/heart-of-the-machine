a8cb57f5be0297b36cc1d9906a374fab2302a3fa
Kneelawk <kneelawk@gmail.com>
2020-06-19 11:30:42 -0700

    Don't override minecraft's dimension checking

--- src/main/java/com/github/hotm/mixinopts/DimensionAdditions.java
+++ src/main/java/com/github/hotm/mixinopts/DimensionAdditions.java
@@ -6,6 +6,8 @@ import net.minecraft.util.registry.SimpleRegistry;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionOptions;
 import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.chunk.SurfaceChunkGenerator;
 
 import java.io.File;
 import java.util.*;
@@ -15,11 +17,11 @@ import java.util.*;
  */
 public class DimensionAdditions {
     private static final List<DimensionAddition> ADDITIONS = new ArrayList<>();
-    private static final Set<RegistryKey<DimensionOptions>> DIMENSION_KEYS = new HashSet<>();
+    private static final Map<RegistryKey<DimensionOptions>, DimensionAddition> DIMENSION_KEYS = new HashMap<>();
     private static final Map<RegistryKey<World>, String> SAVE_DIRS = new HashMap<>();
 
     public static void addDimension(DimensionAddition addition) {
-        DIMENSION_KEYS.add(addition.getOptionsRegistryKey());
+        DIMENSION_KEYS.put(addition.getOptionsRegistryKey(), addition);
         ADDITIONS.add(addition);
     }
 
@@ -54,14 +56,34 @@ public class DimensionAdditions {
         return new File(root, SAVE_DIRS.get(key));
     }
 
-    public static boolean containsOnlyValidKeys(SimpleRegistry<DimensionOptions> registry) {
-        for (Map.Entry<RegistryKey<DimensionOptions>, DimensionOptions> e : registry.getEntries()) {
-            RegistryKey<DimensionOptions> key = e.getKey();
-            if (key != DimensionOptions.OVERWORLD && key != DimensionOptions.NETHER && key != DimensionOptions.END &&
-                    !DIMENSION_KEYS.contains(key)) {
-                return false;
+    public static boolean checkAndRemoveDimensions(long seed,
+            List<Map.Entry<RegistryKey<DimensionOptions>, DimensionOptions>> list) {
+        Iterator<Map.Entry<RegistryKey<DimensionOptions>, DimensionOptions>> it = list.iterator();
+
+        while (it.hasNext()) {
+            Map.Entry<RegistryKey<DimensionOptions>, DimensionOptions> entry = it.next();
+            RegistryKey<DimensionOptions> key = entry.getKey();
+            DimensionOptions options = entry.getValue();
+
+            if (DIMENSION_KEYS.containsKey(key)) {
+                DimensionAddition addition = DIMENSION_KEYS.get(key);
+
+                // do some basic checks to make sure the dimension hasn't been customized
+                if (options.getDimensionType() != addition.getDimensionType()) {
+                    return false;
+                }
+
+                ChunkGenerator generator = addition.getChunkGeneratorSupplier().getChunkGenerator(seed);
+                if (!generator.getClass().isAssignableFrom(options.getChunkGenerator().getClass())) {
+                    return false;
+                }
+
+                // not quite sure how to verify generator settings
+
+                it.remove();
             }
         }
+
         return true;
     }
 }
