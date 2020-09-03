c0a6e6ceaa5f368091a3110aeed1057ac1211eaa
Kneelawk <kneelawk@gmail.com>
2020-09-03 05:29:28 -0700

    Investigate structure namespacing

--- src/main/kotlin/com/github/hotm/gen/feature/HotMStructurePieces.kt
+++ src/main/kotlin/com/github/hotm/gen/feature/HotMStructurePieces.kt
@@ -6,6 +6,7 @@ import net.minecraft.structure.StructurePieceType
  * Manages the Heart of the Machine Structure Pieces.
  */
 object HotMStructurePieces {
+    // NOTE: The id should have been: "hotm:NePP"
     val NECTERE_PORTAL =
         StructurePieceType.register({ manager, tag -> NecterePortalStructureFeature.Piece(manager, tag) }, "HotMNePP")
 
