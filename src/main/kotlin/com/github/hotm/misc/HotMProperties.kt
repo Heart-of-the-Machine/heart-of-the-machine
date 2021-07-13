7d39673fe0327aa1c5ff7efd059ddc64be59be68
Kneelawk <kneelawk@gmail.com>
2021-07-13 15:02:56 -0700

    HotMProperties initialize lazily

--- src/main/kotlin/com/github/hotm/misc/HotMProperties.kt
+++ src/main/kotlin/com/github/hotm/misc/HotMProperties.kt
@@ -4,5 +4,5 @@ import net.minecraft.state.property.IntProperty
 
 object HotMProperties {
     const val MAX_DISTANCE = 16
-    val DISTANCE: IntProperty = IntProperty.of("distance", 1, MAX_DISTANCE)
+    val DISTANCE: IntProperty by lazy { IntProperty.of("distance", 1, MAX_DISTANCE) }
 }
