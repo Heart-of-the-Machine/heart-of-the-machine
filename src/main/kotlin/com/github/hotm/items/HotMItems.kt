23ab3ce0411a5ac58eea15966f5807430669b2af
Kneelawk <kneelawk@gmail.com>
2021-07-13 13:54:56 -0700

    HotMItems initialize lazily until register()

--- src/main/kotlin/com/github/hotm/items/HotMItems.kt
+++ src/main/kotlin/com/github/hotm/items/HotMItems.kt
@@ -8,17 +8,15 @@ import net.minecraft.item.ItemStack
 import net.minecraft.util.registry.Registry
 
 object HotMItems {
-    val HOTM_BUILDING_ITEM_GROUP =
-        FabricItemGroupBuilder.build(HotMConstants.identifier("building"), HotMItems::buildingGroupItem)
-    val HOTM_MATERIAL_ITEM_GROUP =
-        FabricItemGroupBuilder.build(HotMConstants.identifier("materials"), HotMItems::materialGroupItem)
+    val HOTM_BUILDING_ITEM_GROUP by lazy { FabricItemGroupBuilder.build(HotMConstants.identifier("building"), HotMItems::buildingGroupItem) }
+    val HOTM_MATERIAL_ITEM_GROUP by lazy { FabricItemGroupBuilder.build(HotMConstants.identifier("materials"), HotMItems::materialGroupItem) }
 
-    val HOTM_BUILDING_ITEM_SETTINGS: Item.Settings = Item.Settings().group(HOTM_BUILDING_ITEM_GROUP)
-    val HOTM_MATERIAL_ITEM_SETTINGS: Item.Settings = Item.Settings().group(HOTM_MATERIAL_ITEM_GROUP)
+    val HOTM_BUILDING_ITEM_SETTINGS: Item.Settings by lazy { Item.Settings().group(HOTM_BUILDING_ITEM_GROUP) }
+    val HOTM_MATERIAL_ITEM_SETTINGS: Item.Settings by lazy { Item.Settings().group(HOTM_MATERIAL_ITEM_GROUP) }
 
-    val CYAN_CRYSTAL_SHARD = Item(HOTM_MATERIAL_ITEM_SETTINGS)
-    val MAGENTA_CRYSTAL_SHARD = Item(HOTM_MATERIAL_ITEM_SETTINGS)
-    val PLASSEIN_FUEL_CHUNK = Item(HOTM_MATERIAL_ITEM_SETTINGS)
+    val CYAN_CRYSTAL_SHARD by lazy { Item(HOTM_MATERIAL_ITEM_SETTINGS) }
+    val MAGENTA_CRYSTAL_SHARD by lazy { Item(HOTM_MATERIAL_ITEM_SETTINGS) }
+    val PLASSEIN_FUEL_CHUNK by lazy { Item(HOTM_MATERIAL_ITEM_SETTINGS) }
 
     fun register() {
         register(CYAN_CRYSTAL_SHARD, "cyan_crystal_shard")
