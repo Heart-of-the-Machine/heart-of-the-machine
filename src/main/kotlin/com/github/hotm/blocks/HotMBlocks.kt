d8ff4f345b610444ffd42c53c62e09c5ae3a1695
Kneelawk <kneelawk@gmail.com>
2021-07-13 11:59:05 -0700

    HotMBlocks initialize lazily until register()

--- src/main/kotlin/com/github/hotm/blocks/HotMBlocks.kt
+++ src/main/kotlin/com/github/hotm/blocks/HotMBlocks.kt
@@ -1,10 +1,10 @@
 package com.github.hotm.blocks
 
 import com.github.hotm.HotMConstants
-import com.github.hotm.items.HotMItems.HOTM_BUILDING_ITEM_SETTINGS
-import com.github.hotm.items.HotMItems.HOTM_MATERIAL_ITEM_SETTINGS
 import com.github.hotm.blocks.spore.StandardPlasseinSporeGenerator
 import com.github.hotm.items.BracingItem
+import com.github.hotm.items.HotMItems.HOTM_BUILDING_ITEM_SETTINGS
+import com.github.hotm.items.HotMItems.HOTM_MATERIAL_ITEM_SETTINGS
 import com.github.hotm.items.ScaffoldingItem
 import com.github.hotm.mixinapi.BlockCreators
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
@@ -26,161 +26,196 @@ object HotMBlocks {
     /*
      * Block Settings.
      */
-    private val BRACING_SETTINGS =
+    private val BRACING_SETTINGS by lazy {
         FabricBlockSettings.of(Material.METAL, MapColor.GRAY).requiresTool().strength(1.0f, 15.0f)
             .sounds(BlockSoundGroup.METAL).nonOpaque()
-    private val CYAN_CRYSTAL_BLOCK_SETTINGS =
+    }
+    private val CYAN_CRYSTAL_BLOCK_SETTINGS by lazy {
         FabricBlockSettings.of(Material.GLASS, MapColor.CYAN).requiresTool().breakByTool(FabricToolTags.PICKAXES)
             .strength(2.0f, 5.0f).sounds(BlockSoundGroup.GLASS).luminance(15)
-    private val CYAN_LAMP_BLOCK_SETTINGS =
+    }
+    private val CYAN_LAMP_BLOCK_SETTINGS by lazy {
         FabricBlockSettings.of(Material.STONE, MapColor.CYAN).requiresTool().strength(2.0f, 5.0f)
             .sounds(BlockSoundGroup.STONE).luminance(15)
-    private val CYAN_MACHINE_CASING_LAMP_BLOCK_SETTINGS =
+    }
+    private val CYAN_MACHINE_CASING_LAMP_BLOCK_SETTINGS by lazy {
         FabricBlockSettings.of(Material.METAL, MapColor.CYAN).requiresTool().strength(2.0f, 5.0f)
             .sounds(BlockSoundGroup.METAL).luminance(15)
-    private val MACHINE_CASING_SETTINGS =
+    }
+    private val MACHINE_CASING_SETTINGS by lazy {
         FabricBlockSettings.of(Material.METAL, MapColor.BLACK).requiresTool().strength(3.0f, 10.0f)
             .sounds(BlockSoundGroup.METAL)
-    private val MAGENTA_CRYSTAL_BLOCK_SETTINGS =
+    }
+    private val MAGENTA_CRYSTAL_BLOCK_SETTINGS by lazy {
         FabricBlockSettings.of(Material.GLASS, MapColor.MAGENTA).requiresTool().breakByTool(FabricToolTags.PICKAXES)
             .strength(2.0f, 5.0f).sounds(BlockSoundGroup.GLASS).luminance(15)
-    private val MAGENTA_LAMP_BLOCK_SETTINGS =
+    }
+    private val MAGENTA_LAMP_BLOCK_SETTINGS by lazy {
         FabricBlockSettings.of(Material.STONE, MapColor.MAGENTA).requiresTool().strength(2.0f, 5.0f)
             .sounds(BlockSoundGroup.STONE).luminance(15)
-    private val MAGENTA_MACHINE_CASING_LAMP_BLOCK_SETTINGS =
+    }
+    private val MAGENTA_MACHINE_CASING_LAMP_BLOCK_SETTINGS by lazy {
         FabricBlockSettings.of(Material.METAL, MapColor.MAGENTA).requiresTool().strength(2.0f, 5.0f)
             .sounds(BlockSoundGroup.METAL).luminance(15)
-    private val OBELISK_PART_SETTINGS =
+    }
+    private val OBELISK_PART_SETTINGS by lazy {
         FabricBlockSettings.of(Material.METAL, MapColor.DARK_AQUA).requiresTool().strength(3.0f, 10.0f)
             .sounds(BlockSoundGroup.STONE)
-    private val PLASSEIN_LOG_SETTINGS =
+    }
+    private val PLASSEIN_LOG_SETTINGS by lazy {
         FabricBlockSettings.of(Material.WOOD, MapColor.BLUE).breakByTool(FabricToolTags.AXES).strength(1.0f, 10.0f)
             .sounds(BlockSoundGroup.WOOD)
-    private val THINKING_STONE_SETTINGS =
+    }
+    private val THINKING_STONE_SETTINGS by lazy {
         FabricBlockSettings.of(Material.STONE, MapColor.BLACK).requiresTool().strength(3.0f, 10.0f)
             .sounds(BlockSoundGroup.STONE)
+    }
 
 
     /*
      * Leyline blocks.
      */
     private val LEYLINE_BLOCKS = hashSetOf<Block>()
-    val MACHINE_CASING_LEYLINE = addLeyline(Block(MACHINE_CASING_SETTINGS))
-    val PLASSEIN_GRASS_LEYLINE = addLeyline(Block(MACHINE_CASING_SETTINGS))
-    val PLASSEIN_LOG_LEYLINE = addLeyline(PillarBlock(PLASSEIN_LOG_SETTINGS))
-    val RUSTED_MACHINE_CASING_LEYLINE = addLeyline(Block(MACHINE_CASING_SETTINGS))
-    val SMOOTH_THINKING_STONE_LEYLINE = addLeyline(Block(THINKING_STONE_SETTINGS))
-    val SURFACE_MACHINE_CASING_LEYLINE = addLeyline(Block(MACHINE_CASING_SETTINGS))
-    val THINKING_STONE_LEYLINE = addLeyline(Block(THINKING_STONE_SETTINGS))
+    val MACHINE_CASING_LEYLINE by lazy { addLeyline(Block(MACHINE_CASING_SETTINGS)) }
+    val PLASSEIN_GRASS_LEYLINE by lazy { addLeyline(Block(MACHINE_CASING_SETTINGS)) }
+    val PLASSEIN_LOG_LEYLINE by lazy { addLeyline(PillarBlock(PLASSEIN_LOG_SETTINGS)) }
+    val RUSTED_MACHINE_CASING_LEYLINE by lazy { addLeyline(Block(MACHINE_CASING_SETTINGS)) }
+    val SMOOTH_THINKING_STONE_LEYLINE by lazy { addLeyline(Block(THINKING_STONE_SETTINGS)) }
+    val SURFACE_MACHINE_CASING_LEYLINE by lazy { addLeyline(Block(MACHINE_CASING_SETTINGS)) }
+    val THINKING_STONE_LEYLINE by lazy { addLeyline(Block(THINKING_STONE_SETTINGS)) }
 
     /*
      * Crystals & Lamps.
      */
-    val CYAN_CRYSTAL = Block(CYAN_CRYSTAL_BLOCK_SETTINGS)
-    val CYAN_CRYSTAL_LAMP = Block(CYAN_LAMP_BLOCK_SETTINGS)
-    val CYAN_MACHINE_CASING_LAMP = Block(CYAN_MACHINE_CASING_LAMP_BLOCK_SETTINGS)
-    val CYAN_THINKING_STONE_LAMP = Block(CYAN_LAMP_BLOCK_SETTINGS)
-    val MAGENTA_CRYSTAL = Block(MAGENTA_CRYSTAL_BLOCK_SETTINGS)
-    val MAGENTA_CRYSTAL_LAMP = Block(MAGENTA_LAMP_BLOCK_SETTINGS)
-    val MAGENTA_MACHINE_CASING_LAMP = Block(MAGENTA_MACHINE_CASING_LAMP_BLOCK_SETTINGS)
-    val MAGENTA_THINKING_STONE_LAMP = Block(MAGENTA_LAMP_BLOCK_SETTINGS)
+    val CYAN_CRYSTAL by lazy { Block(CYAN_CRYSTAL_BLOCK_SETTINGS) }
+    val CYAN_CRYSTAL_LAMP by lazy { Block(CYAN_LAMP_BLOCK_SETTINGS) }
+    val CYAN_MACHINE_CASING_LAMP by lazy { Block(CYAN_MACHINE_CASING_LAMP_BLOCK_SETTINGS) }
+    val CYAN_THINKING_STONE_LAMP by lazy { Block(CYAN_LAMP_BLOCK_SETTINGS) }
+    val MAGENTA_CRYSTAL by lazy { Block(MAGENTA_CRYSTAL_BLOCK_SETTINGS) }
+    val MAGENTA_CRYSTAL_LAMP by lazy { Block(MAGENTA_LAMP_BLOCK_SETTINGS) }
+    val MAGENTA_MACHINE_CASING_LAMP by lazy { Block(MAGENTA_MACHINE_CASING_LAMP_BLOCK_SETTINGS) }
+    val MAGENTA_THINKING_STONE_LAMP by lazy { Block(MAGENTA_LAMP_BLOCK_SETTINGS) }
 
     /*
      * Obelisk parts.
      */
-    val GLOWY_OBELISK_PART = PillarBlock(OBELISK_PART_SETTINGS)
-    val OBELISK_PART = PillarBlock(OBELISK_PART_SETTINGS)
-    val NECTERE_PORTAL = NecterePortalBlock(
-        FabricBlockSettings.of(Material.PORTAL, MapColor.CYAN).noCollision().strength(-1.0f)
-            .sounds(BlockSoundGroup.GLASS).nonOpaque().luminance(12)
-    )
-    val NECTERE_PORTAL_SPAWNER =
+    val GLOWY_OBELISK_PART by lazy { PillarBlock(OBELISK_PART_SETTINGS) }
+    val OBELISK_PART by lazy { PillarBlock(OBELISK_PART_SETTINGS) }
+    val NECTERE_PORTAL by lazy {
+        NecterePortalBlock(
+            FabricBlockSettings.of(Material.PORTAL, MapColor.CYAN).noCollision().strength(-1.0f)
+                .sounds(BlockSoundGroup.GLASS).nonOpaque().luminance(12)
+        )
+    }
+    val NECTERE_PORTAL_SPAWNER by lazy {
         NecterePortalSpawnerBlock(FabricBlockSettings.of(Material.STONE, MapColor.DARK_AQUA).strength(-1.0f))
+    }
 
     /*
      * Machine Casing Blocks.
      */
-    val MACHINE_CASING = LeylineableBlock(MACHINE_CASING_LEYLINE, MACHINE_CASING_SETTINGS)
-    val MACHINE_CASING_BRICKS = Block(MACHINE_CASING_SETTINGS)
-    val METAL_MACHINE_CASING = Block(MACHINE_CASING_SETTINGS)
-    val PLASSEIN_MACHINE_CASING = Block(MACHINE_CASING_SETTINGS)
-    val RUSTED_MACHINE_CASING = LeylineableBlock(RUSTED_MACHINE_CASING_LEYLINE, MACHINE_CASING_SETTINGS)
-    val SURFACE_MACHINE_CASING = LeylineableBlock(SURFACE_MACHINE_CASING_LEYLINE, MACHINE_CASING_SETTINGS)
-    val TEST_MACHINE_CASING = Block(MACHINE_CASING_SETTINGS)
-    val MACHINE_CASING_STAIRS = BlockCreators.createStairsBlock(MACHINE_CASING.defaultState, MACHINE_CASING_SETTINGS)
-    val MACHINE_CASING_BRICK_STAIRS =
+    val MACHINE_CASING by lazy { LeylineableBlock(MACHINE_CASING_LEYLINE, MACHINE_CASING_SETTINGS) }
+    val MACHINE_CASING_BRICKS by lazy { Block(MACHINE_CASING_SETTINGS) }
+    val METAL_MACHINE_CASING by lazy { Block(MACHINE_CASING_SETTINGS) }
+    val PLASSEIN_MACHINE_CASING by lazy { Block(MACHINE_CASING_SETTINGS) }
+    val RUSTED_MACHINE_CASING by lazy { LeylineableBlock(RUSTED_MACHINE_CASING_LEYLINE, MACHINE_CASING_SETTINGS) }
+    val SURFACE_MACHINE_CASING by lazy { LeylineableBlock(SURFACE_MACHINE_CASING_LEYLINE, MACHINE_CASING_SETTINGS) }
+    val TEST_MACHINE_CASING by lazy { Block(MACHINE_CASING_SETTINGS) }
+    val MACHINE_CASING_STAIRS by lazy {
+        BlockCreators.createStairsBlock(MACHINE_CASING.defaultState, MACHINE_CASING_SETTINGS)
+    }
+    val MACHINE_CASING_BRICK_STAIRS by lazy {
         BlockCreators.createStairsBlock(MACHINE_CASING_BRICKS.defaultState, MACHINE_CASING_SETTINGS)
-    val MACHINE_CASING_SLAB = SlabBlock(MACHINE_CASING_SETTINGS)
-    val MACHINE_CASING_BRICK_SLAB = SlabBlock(MACHINE_CASING_SETTINGS)
+    }
+    val MACHINE_CASING_SLAB by lazy { SlabBlock(MACHINE_CASING_SETTINGS) }
+    val MACHINE_CASING_BRICK_SLAB by lazy { SlabBlock(MACHINE_CASING_SETTINGS) }
 
     /*
      * Bracing Blocks.
      */
-    val METAL_BRACING = BracingBlock(BRACING_SETTINGS)
-    val PLASSEIN_BRACING = BracingBlock(BRACING_SETTINGS)
+    val METAL_BRACING by lazy { BracingBlock(BRACING_SETTINGS) }
+    val PLASSEIN_BRACING by lazy { BracingBlock(BRACING_SETTINGS) }
 
     /*
      * Plassein Blocks.
      */
-    val PLASSEIN_BLOOM = Block(
-        FabricBlockSettings.of(Material.LEAVES, MapColor.BLUE).strength(1.0f, 10.0f).sounds(BlockSoundGroup.WOOL)
-            .nonOpaque().allowsSpawning(HotMBlocks::never)
-    )
-    val PLASSEIN_FUEL_BLOCK =
-        Block(FabricBlockSettings.of(Material.STONE, MapColor.BLACK).requiresTool().strength(5.0F, 6.0F))
-    val PLASSEIN_GRASS = LeylineableBlock(PLASSEIN_GRASS_LEYLINE, MACHINE_CASING_SETTINGS)
-    val PLASSEIN_LEAVES = PlasseinLeavesBlock(
-        FabricBlockSettings.of(Material.LEAVES, MapColor.CYAN).strength(0.2f).ticksRandomly()
-            .sounds(BlockSoundGroup.GRASS).nonOpaque().allowsSpawning(HotMBlocks::never).suffocates(HotMBlocks::never)
-            .blockVision(HotMBlocks::never)
-    )
-    val PLASSEIN_LOG = LeylineablePillarBlock(PLASSEIN_LOG_LEYLINE, PLASSEIN_LOG_SETTINGS)
-    val PLASSEIN_PLANKS = Block(PLASSEIN_LOG_SETTINGS)
-    val PLASSEIN_SCAFFOLDING = ScaffoldingBlock(
-        FabricBlockSettings.of(Material.DECORATION, MapColor.BLUE).noCollision().sounds(BlockSoundGroup.SCAFFOLDING)
-            .dynamicBounds()
-    )
-    val PLASSEIN_SPORE = PlasseinSporeBlock(
-        StandardPlasseinSporeGenerator,
-        FabricBlockSettings.of(Material.PLANT, MapColor.BLUE).noCollision().ticksRandomly().breakInstantly()
-            .sounds(BlockSoundGroup.GRASS)
-    )
-    val PLASSEIN_STEM = PillarBlock(PLASSEIN_LOG_SETTINGS)
+    val PLASSEIN_BLOOM by lazy {
+        Block(
+            FabricBlockSettings.of(Material.LEAVES, MapColor.BLUE).strength(1.0f, 10.0f).sounds(BlockSoundGroup.WOOL)
+                .nonOpaque().allowsSpawning(HotMBlocks::never)
+        )
+    }
+    val PLASSEIN_FUEL_BLOCK by lazy {
+        Block(
+            FabricBlockSettings.of(Material.STONE, MapColor.BLACK).requiresTool().strength(5.0F, 6.0F)
+        )
+    }
+    val PLASSEIN_GRASS by lazy { LeylineableBlock(PLASSEIN_GRASS_LEYLINE, MACHINE_CASING_SETTINGS) }
+    val PLASSEIN_LEAVES by lazy {
+        PlasseinLeavesBlock(
+            FabricBlockSettings.of(Material.LEAVES, MapColor.CYAN).strength(0.2f).ticksRandomly()
+                .sounds(BlockSoundGroup.GRASS).nonOpaque().allowsSpawning(HotMBlocks::never)
+                .suffocates(HotMBlocks::never).blockVision(HotMBlocks::never)
+        )
+    }
+    val PLASSEIN_LOG by lazy { LeylineablePillarBlock(PLASSEIN_LOG_LEYLINE, PLASSEIN_LOG_SETTINGS) }
+    val PLASSEIN_PLANKS by lazy { Block(PLASSEIN_LOG_SETTINGS) }
+    val PLASSEIN_SCAFFOLDING by lazy {
+        ScaffoldingBlock(
+            FabricBlockSettings.of(Material.DECORATION, MapColor.BLUE).noCollision().sounds(BlockSoundGroup.SCAFFOLDING)
+                .dynamicBounds()
+        )
+    }
+    val PLASSEIN_SPORE by lazy {
+        PlasseinSporeBlock(
+            StandardPlasseinSporeGenerator,
+            FabricBlockSettings.of(Material.PLANT, MapColor.BLUE).noCollision().ticksRandomly().breakInstantly()
+                .sounds(BlockSoundGroup.GRASS)
+        )
+    }
+    val PLASSEIN_STEM by lazy { PillarBlock(PLASSEIN_LOG_SETTINGS) }
 
     /*
      * Thinking Stone Blocks.
      */
-    val SMOOTH_THINKING_STONE = LeylineableBlock(SMOOTH_THINKING_STONE_LEYLINE, THINKING_STONE_SETTINGS)
-    val THINKING_STONE = LeylineableBlock(THINKING_STONE_LEYLINE, THINKING_STONE_SETTINGS)
-    val THINKING_STONE_BRICKS = Block(THINKING_STONE_SETTINGS)
-    val THINKING_STONE_TILES = Block(THINKING_STONE_SETTINGS)
-    val SMOOTH_THINKING_STONE_STAIRS =
+    val SMOOTH_THINKING_STONE by lazy { LeylineableBlock(SMOOTH_THINKING_STONE_LEYLINE, THINKING_STONE_SETTINGS) }
+    val THINKING_STONE by lazy { LeylineableBlock(THINKING_STONE_LEYLINE, THINKING_STONE_SETTINGS) }
+    val THINKING_STONE_BRICKS by lazy { Block(THINKING_STONE_SETTINGS) }
+    val THINKING_STONE_TILES by lazy { Block(THINKING_STONE_SETTINGS) }
+    val SMOOTH_THINKING_STONE_STAIRS by lazy {
         BlockCreators.createStairsBlock(SMOOTH_THINKING_STONE.defaultState, THINKING_STONE_SETTINGS)
-    val THINKING_STONE_STAIRS = BlockCreators.createStairsBlock(THINKING_STONE.defaultState, THINKING_STONE_SETTINGS)
-    val THINKING_STONE_BRICK_STAIRS =
+    }
+    val THINKING_STONE_STAIRS by lazy {
+        BlockCreators.createStairsBlock(THINKING_STONE.defaultState, THINKING_STONE_SETTINGS)
+    }
+    val THINKING_STONE_BRICK_STAIRS by lazy {
         BlockCreators.createStairsBlock(THINKING_STONE_BRICKS.defaultState, THINKING_STONE_SETTINGS)
-    val THINKING_STONE_TILE_STAIRS =
+    }
+    val THINKING_STONE_TILE_STAIRS by lazy {
         BlockCreators.createStairsBlock(THINKING_STONE_TILES.defaultState, THINKING_STONE_SETTINGS)
-    val SMOOTH_THINKING_STONE_SLAB = SlabBlock(THINKING_STONE_SETTINGS)
-    val THINKING_STONE_SLAB = SlabBlock(THINKING_STONE_SETTINGS)
-    val THINKING_STONE_BRICK_SLAB = SlabBlock(THINKING_STONE_SETTINGS)
-    val THINKING_STONE_TILE_SLAB = SlabBlock(THINKING_STONE_SETTINGS)
+    }
+    val SMOOTH_THINKING_STONE_SLAB by lazy { SlabBlock(THINKING_STONE_SETTINGS) }
+    val THINKING_STONE_SLAB by lazy { SlabBlock(THINKING_STONE_SETTINGS) }
+    val THINKING_STONE_BRICK_SLAB by lazy { SlabBlock(THINKING_STONE_SETTINGS) }
+    val THINKING_STONE_TILE_SLAB by lazy { SlabBlock(THINKING_STONE_SETTINGS) }
 
     /*
      * Sand blocks.
      */
-    private val SAND_SETTINGS =
+    private val SAND_SETTINGS by lazy {
         FabricBlockSettings.of(Material.AGGREGATE, MapColor.DEEPSLATE_GRAY).strength(0.5F).sounds(BlockSoundGroup.SAND)
-    val NULL_SAND = SandBlock(0x29261d, SAND_SETTINGS)
+    }
+    val NULL_SAND by lazy { SandBlock(0x29261d, SAND_SETTINGS) }
 
     /*
      * Thinking glass blocks.
      */
-    private val GLASS_SETTINGS =
+    private val GLASS_SETTINGS by lazy {
         FabricBlockSettings.of(Material.GLASS, MapColor.BLACK).strength(0.5f, 10.0f).sounds(BlockSoundGroup.GLASS)
             .nonOpaque().allowsSpawning(HotMBlocks::never).solidBlock(HotMBlocks::never).suffocates(HotMBlocks::never)
             .blockVision(HotMBlocks::never)
-    val THINKING_GLASS = GlassBlock(GLASS_SETTINGS)
+    }
+    val THINKING_GLASS by lazy { GlassBlock(GLASS_SETTINGS) }
 
     /**
      * Register all Heart of the Machine blocks...
