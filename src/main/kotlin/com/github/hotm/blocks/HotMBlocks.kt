1abe2eb0238f7209a32f30192b0bea8c334dea33
Kneelawk <kneelawk@gmail.com>
2021-08-04 15:42:14 -0700

    Initial portal transmitter aura node design

--- src/main/kotlin/com/github/hotm/blocks/HotMBlocks.kt
+++ src/main/kotlin/com/github/hotm/blocks/HotMBlocks.kt
@@ -228,6 +228,7 @@ object HotMBlocks {
     val BASIC_SIPHON_AURA_NODE by lazy { BasicSiphonAuraNodeBlock(AURA_NODE_SETTINGS) }
     val BASIC_SOURCE_AURA_NODE by lazy { BasicSourceAuraNodeBlock(AURA_NODE_SETTINGS) }
     val COLLECTOR_DISTRIBUTOR_AURA_NODE by lazy { CollectorDistributorAuraNodeBlock(AURA_NODE_SETTINGS) }
+    val PORTAL_TRANSMITTER_AURA_NODE by lazy { Block(AURA_NODE_SETTINGS) }
 
     /*
      * Debug Blocks.
@@ -285,6 +286,7 @@ object HotMBlocks {
         )
         register(PLASSEIN_SPORE, "plassein_spore", HOTM_BUILDING_ITEM_SETTINGS)
         register(PLASSEIN_STEM, "plassein_stem", HOTM_BUILDING_ITEM_SETTINGS)
+        register(PORTAL_TRANSMITTER_AURA_NODE, "portal_transmitter_aura_node", HOTM_MACHINE_ITEM_SETTINGS)
         register(RUSTED_MACHINE_CASING, "rusted_machine_casing", HOTM_BUILDING_ITEM_SETTINGS)
         register(RUSTED_MACHINE_CASING_LEYLINE, "rusted_machine_casing_leyline", HOTM_BUILDING_ITEM_SETTINGS)
         register(SMOOTH_THINKING_STONE, "smooth_thinking_stone", HOTM_BUILDING_ITEM_SETTINGS)
