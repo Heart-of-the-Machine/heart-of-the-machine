f57b5a1de67f17685bc2128896a5becdd0149fff
Kneelawk <jedidiah.pommert@gmail.com>
2021-08-26 21:48:43 -0700

    Support leyline decals for rotatable blocks

--- src/main/kotlin/com/github/hotm/client/blockmodel/QuadEmitterUtils.kt
+++ src/main/kotlin/com/github/hotm/client/blockmodel/QuadEmitterUtils.kt
@@ -3,15 +3,35 @@ package com.github.hotm.client.blockmodel
 import net.fabricmc.fabric.api.renderer.v1.mesh.QuadEmitter
 import net.minecraft.client.render.model.ModelBakeSettings
 import net.minecraft.util.math.Direction
+import net.minecraft.util.math.Vec2f
+import net.minecraft.util.math.Vec3f
 import net.minecraft.util.math.Vector4f
+import kotlin.math.PI
 import kotlin.math.abs
+import kotlin.math.atan2
 
 object QuadEmitterUtils {
     const val ORIGIN_X = 0.5f
     const val ORIGIN_Y = 0.5f
     const val ORIGIN_Z = 0.5f
 
-    fun square(
+    private data class Vertex(var vec: Vector4f, val uv: Vec2f) {
+        fun angleFromTop(nominalFace: Direction, center: Vec3f): Float {
+            val x = vec.x - center.x
+            val y = vec.y - center.y
+            val z = vec.z - center.z
+            return when (nominalFace) {
+                Direction.DOWN -> atan2(x, -z)
+                Direction.UP -> atan2(x, z)
+                Direction.NORTH -> atan2(-x, -y)
+                Direction.SOUTH -> atan2(x, -y)
+                Direction.WEST -> atan2(z, -y)
+                Direction.EAST -> atan2(-z, -y)
+            }
+        }
+    }
+
+    fun uvSquare(
         emitter: QuadEmitter,
         rotationContainer: ModelBakeSettings,
         nominalFace: Direction,
@@ -32,58 +52,85 @@ object QuadEmitterUtils {
 
         emitter.nominalFace(transformedFace)
 
-        val vec0: Vector4f
-        val vec1: Vector4f
-        val vec2: Vector4f
-        val vec3: Vector4f
+        val verts = arrayOf(
+            Vertex(Vector4f(), Vec2f(left, 1.0f - top)),
+            Vertex(Vector4f(), Vec2f(left, 1.0f - bottom)),
+            Vertex(Vector4f(), Vec2f(right, 1.0f - bottom)),
+            Vertex(Vector4f(), Vec2f(right, 1.0f - top))
+        )
 
         when (nominalFace) {
             Direction.DOWN -> {
-                vec0 = Vector4f(left - ORIGIN_X, depth - ORIGIN_Y, top - ORIGIN_Z, 1f)
-                vec1 = Vector4f(left - ORIGIN_X, depth - ORIGIN_Y, bottom - ORIGIN_Z, 1f)
-                vec2 = Vector4f(right - ORIGIN_X, depth - ORIGIN_Y, bottom - ORIGIN_Z, 1f)
-                vec3 = Vector4f(right - ORIGIN_X, depth - ORIGIN_Y, top - ORIGIN_Z, 1f)
+                verts[0].vec = Vector4f(left - ORIGIN_X, depth - ORIGIN_Y, top - ORIGIN_Z, 1f)
+                verts[1].vec = Vector4f(left - ORIGIN_X, depth - ORIGIN_Y, bottom - ORIGIN_Z, 1f)
+                verts[2].vec = Vector4f(right - ORIGIN_X, depth - ORIGIN_Y, bottom - ORIGIN_Z, 1f)
+                verts[3].vec = Vector4f(right - ORIGIN_X, depth - ORIGIN_Y, top - ORIGIN_Z, 1f)
             }
             Direction.UP -> {
-                vec0 = Vector4f(left - ORIGIN_X, 1f - depth - ORIGIN_Y, 1f - top - ORIGIN_Z, 1f)
-                vec1 = Vector4f(left - ORIGIN_X, 1f - depth - ORIGIN_Y, 1f - bottom - ORIGIN_Z, 1f)
-                vec2 = Vector4f(right - ORIGIN_X, 1f - depth - ORIGIN_Y, 1f - bottom - ORIGIN_Z, 1f)
-                vec3 = Vector4f(right - ORIGIN_X, 1f - depth - ORIGIN_Y, 1f - top - ORIGIN_Z, 1f)
+                verts[0].vec = Vector4f(left - ORIGIN_X, 1f - depth - ORIGIN_Y, 1f - top - ORIGIN_Z, 1f)
+                verts[1].vec = Vector4f(left - ORIGIN_X, 1f - depth - ORIGIN_Y, 1f - bottom - ORIGIN_Z, 1f)
+                verts[2].vec = Vector4f(right - ORIGIN_X, 1f - depth - ORIGIN_Y, 1f - bottom - ORIGIN_Z, 1f)
+                verts[3].vec = Vector4f(right - ORIGIN_X, 1f - depth - ORIGIN_Y, 1f - top - ORIGIN_Z, 1f)
             }
             Direction.NORTH -> {
-                vec0 = Vector4f(right - ORIGIN_X, top - ORIGIN_Y, depth - ORIGIN_Z, 1f)
-                vec1 = Vector4f(right - ORIGIN_X, bottom - ORIGIN_Y, depth - ORIGIN_Z, 1f)
-                vec2 = Vector4f(left - ORIGIN_X, bottom - ORIGIN_Y, depth - ORIGIN_Z, 1f)
-                vec3 = Vector4f(left - ORIGIN_X, top - ORIGIN_Y, depth - ORIGIN_Z, 1f)
+                verts[0].vec = Vector4f(1.0f - left - ORIGIN_X, top - ORIGIN_Y, depth - ORIGIN_Z, 1f)
+                verts[1].vec = Vector4f(1.0f - left - ORIGIN_X, bottom - ORIGIN_Y, depth - ORIGIN_Z, 1f)
+                verts[2].vec = Vector4f(1.0f - right - ORIGIN_X, bottom - ORIGIN_Y, depth - ORIGIN_Z, 1f)
+                verts[3].vec = Vector4f(1.0f - right - ORIGIN_X, top - ORIGIN_Y, depth - ORIGIN_Z, 1f)
             }
             Direction.SOUTH -> {
-                vec0 = Vector4f(1f - right - ORIGIN_X, top - ORIGIN_Y, 1f - depth - ORIGIN_Z, 1f)
-                vec1 = Vector4f(1f - right - ORIGIN_X, bottom - ORIGIN_Y, 1f - depth - ORIGIN_Z, 1f)
-                vec2 = Vector4f(1f - left - ORIGIN_X, bottom - ORIGIN_Y, 1f - depth - ORIGIN_Z, 1f)
-                vec3 = Vector4f(1f - left - ORIGIN_X, top - ORIGIN_Y, 1f - depth - ORIGIN_Z, 1f)
+                verts[0].vec = Vector4f(left - ORIGIN_X, top - ORIGIN_Y, 1f - depth - ORIGIN_Z, 1f)
+                verts[1].vec = Vector4f(left - ORIGIN_X, bottom - ORIGIN_Y, 1f - depth - ORIGIN_Z, 1f)
+                verts[2].vec = Vector4f(right - ORIGIN_X, bottom - ORIGIN_Y, 1f - depth - ORIGIN_Z, 1f)
+                verts[3].vec = Vector4f(right - ORIGIN_X, top - ORIGIN_Y, 1f - depth - ORIGIN_Z, 1f)
             }
             Direction.WEST -> {
-                vec0 = Vector4f(depth - ORIGIN_X, top - ORIGIN_Y, left - ORIGIN_Z, 1f)
-                vec1 = Vector4f(depth - ORIGIN_X, bottom - ORIGIN_Y, left - ORIGIN_Z, 1f)
-                vec2 = Vector4f(depth - ORIGIN_X, bottom - ORIGIN_Y, right - ORIGIN_Z, 1f)
-                vec3 = Vector4f(depth - ORIGIN_X, top - ORIGIN_Y, right - ORIGIN_Z, 1f)
+                verts[0].vec = Vector4f(depth - ORIGIN_X, top - ORIGIN_Y, left - ORIGIN_Z, 1f)
+                verts[1].vec = Vector4f(depth - ORIGIN_X, bottom - ORIGIN_Y, left - ORIGIN_Z, 1f)
+                verts[2].vec = Vector4f(depth - ORIGIN_X, bottom - ORIGIN_Y, right - ORIGIN_Z, 1f)
+                verts[3].vec = Vector4f(depth - ORIGIN_X, top - ORIGIN_Y, right - ORIGIN_Z, 1f)
             }
             Direction.EAST -> {
-                vec0 = Vector4f(1f - depth - ORIGIN_X, top - ORIGIN_Y, 1f - left - ORIGIN_Z, 1f)
-                vec1 = Vector4f(1f - depth - ORIGIN_X, bottom - ORIGIN_Y, 1f - left - ORIGIN_Z, 1f)
-                vec2 = Vector4f(1f - depth - ORIGIN_X, bottom - ORIGIN_Y, 1f - right - ORIGIN_Z, 1f)
-                vec3 = Vector4f(1f - depth - ORIGIN_X, top - ORIGIN_Y, 1f - right - ORIGIN_Z, 1f)
+                verts[0].vec = Vector4f(1f - depth - ORIGIN_X, top - ORIGIN_Y, 1f - left - ORIGIN_Z, 1f)
+                verts[1].vec = Vector4f(1f - depth - ORIGIN_X, bottom - ORIGIN_Y, 1f - left - ORIGIN_Z, 1f)
+                verts[2].vec = Vector4f(1f - depth - ORIGIN_X, bottom - ORIGIN_Y, 1f - right - ORIGIN_Z, 1f)
+                verts[3].vec = Vector4f(1f - depth - ORIGIN_X, top - ORIGIN_Y, 1f - right - ORIGIN_Z, 1f)
             }
         }
 
-        vec0.transform(rotationContainer.rotation.matrix)
-        vec1.transform(rotationContainer.rotation.matrix)
-        vec2.transform(rotationContainer.rotation.matrix)
-        vec3.transform(rotationContainer.rotation.matrix)
+        verts[0].vec.transform(rotationContainer.rotation.matrix)
+        verts[1].vec.transform(rotationContainer.rotation.matrix)
+        verts[2].vec.transform(rotationContainer.rotation.matrix)
+        verts[3].vec.transform(rotationContainer.rotation.matrix)
 
-        emitter.pos(0, vec0.x + ORIGIN_X, vec0.y + ORIGIN_Y, vec0.z + ORIGIN_Z)
-        emitter.pos(1, vec1.x + ORIGIN_X, vec1.y + ORIGIN_Y, vec1.z + ORIGIN_Z)
-        emitter.pos(2, vec2.x + ORIGIN_X, vec2.y + ORIGIN_Y, vec2.z + ORIGIN_Z)
-        emitter.pos(3, vec3.x + ORIGIN_X, vec3.y + ORIGIN_Y, vec3.z + ORIGIN_Z)
+        val center = Vec3f()
+        for (vert in verts) {
+            center.add(vert.vec.x, vert.vec.y, vert.vec.z)
+        }
+        center.scale(0.25f)
+
+        var initIndex = 0
+        for (i in 0 until 4) {
+            val angle = verts[i].angleFromTop(transformedFace, center)
+            if (angle < -PI.toFloat() / 2f) {
+                initIndex = i
+                break
+            }
+        }
+
+        val vert0 = verts[initIndex]
+        val vert1 = verts[(initIndex + 1) % 4]
+        val vert2 = verts[(initIndex + 2) % 4]
+        val vert3 = verts[(initIndex + 3) % 4]
+
+        emitter.pos(0, vert0.vec.x + ORIGIN_X, vert0.vec.y + ORIGIN_Y, vert0.vec.z + ORIGIN_Z)
+        emitter.pos(1, vert1.vec.x + ORIGIN_X, vert1.vec.y + ORIGIN_Y, vert1.vec.z + ORIGIN_Z)
+        emitter.pos(2, vert2.vec.x + ORIGIN_X, vert2.vec.y + ORIGIN_Y, vert2.vec.z + ORIGIN_Z)
+        emitter.pos(3, vert3.vec.x + ORIGIN_X, vert3.vec.y + ORIGIN_Y, vert3.vec.z + ORIGIN_Z)
+
+        emitter.sprite(0, 0, vert0.uv)
+        emitter.sprite(1, 0, vert1.uv)
+        emitter.sprite(2, 0, vert2.uv)
+        emitter.sprite(3, 0, vert3.uv)
     }
 }
--- src/main/kotlin/com/github/hotm/client/blockmodel/util/QuadPos.kt
+++ src/main/kotlin/com/github/hotm/client/blockmodel/util/QuadPos.kt
@@ -8,13 +8,13 @@ import net.minecraft.util.math.Direction
 data class QuadPos(val left: Float, val bottom: Float, val right: Float, val top: Float, val depth: Float) {
     fun emit(emitter: QuadEmitter, face: Direction, rotationContainer: ModelBakeSettings?) {
         if (rotationContainer != null) {
-            QuadEmitterUtils.square(emitter, rotationContainer, face, left, bottom, right, top, depth)
+            QuadEmitterUtils.uvSquare(emitter, rotationContainer, face, left, bottom, right, top, depth)
         } else {
             emitter.square(face, left, bottom, right, top, depth)
+            emitter.sprite(0, 0, left, 1.0f - top)
+            emitter.sprite(1, 0, left, 1.0f - bottom)
+            emitter.sprite(2, 0, right, 1.0f - bottom)
+            emitter.sprite(3, 0, right, 1.0f - top)
         }
-        emitter.sprite(0, 0, left, 1.0f - top)
-        emitter.sprite(1, 0, left, 1.0f - bottom)
-        emitter.sprite(2, 0, right, 1.0f - bottom)
-        emitter.sprite(3, 0, right, 1.0f - top)
     }
 }
