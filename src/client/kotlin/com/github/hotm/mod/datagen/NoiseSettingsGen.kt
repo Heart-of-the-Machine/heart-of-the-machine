e47e5d2e90c1a19bc29c6c33c108cef4eb37fd96
Kneelawk <kneelawk@gmail.com>
2023-05-04 18:22:33 -0700

    Clean up noise settings generator

--- src/client/kotlin/com/github/hotm/mod/datagen/NoiseSettingsGen.kt
+++ src/client/kotlin/com/github/hotm/mod/datagen/NoiseSettingsGen.kt
@@ -2,30 +2,20 @@ package com.github.hotm.mod.datagen
 
 import com.github.hotm.mod.Constants.id
 import com.github.hotm.mod.block.HotMBlocks
-import com.github.hotm.mod.datagen.noise.*
+import com.github.hotm.mod.datagen.noise.NoiseSettingsProvider
+import com.github.hotm.mod.datagen.noise.df
+import com.github.hotm.mod.datagen.noise.shiftedNoise
+import com.github.hotm.mod.datagen.noise.yGradient
+import com.github.hotm.mod.util.asBiomeKey
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.minecraft.block.Blocks
-import net.minecraft.registry.RegistryKey
-import net.minecraft.registry.RegistryKeys
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.VerticalSurfaceType
 import net.minecraft.world.gen.YOffset
-import net.minecraft.world.gen.chunk.GenerationShapeConfig
 import net.minecraft.world.gen.surfacebuilder.SurfaceRules.*
 
 class NoiseSettingsGen(output: FabricDataOutput) : NoiseSettingsProvider(output) {
     override fun generate() {
-        val upperBoundedCaves =
-            0.9375.df + yGradient(160, 224, 1.0, 0.85) * (id("nectere/cave_3d_noise").df - 0.9375.df)
-        val caves = 2.5.df - 0.15.df + yGradient(-72, -40, 0.0, 1.0) * (upperBoundedCaves - 2.5.df)
-        val upperBoundedSurface =
-            yGradient(192, 256, 1.0, 0.0) * (0.9375.df + id("nectere/surface_3d_noise").df) - 0.9375.df
-        val surface = 2.5.df + yGradient(160, 224, 0.65, 1.0) * (upperBoundedSurface - 2.5.df)
-
-        val finalDensity =
-            ((yGradient(160, 224, 1.0, 0.0) * caves + yGradient(160, 224, 0.0, 1.0) * surface).blendDensity()
-                .interpolated() * 0.64.df).squeeze()
-
         noiseSettings(id("nectere")) {
             seaLevel(0)
             disableMobGeneration(false)
@@ -51,7 +41,16 @@ class NoiseSettingsGen(output: FabricDataOutput) : NoiseSettingsProvider(output)
                     shiftZ = "minecraft:shift_z".df
                 )
 
-                finalDensity(finalDensity)
+                val upperBoundedCaves =
+                    0.9375.df + yGradient(160, 224, 1.0, 0.85) * (id("nectere/cave_3d_noise").df - 0.9375.df)
+                val caves = 2.5.df - 0.15.df + yGradient(-72, -40, 0.0, 1.0) * (upperBoundedCaves - 2.5.df)
+                val upperBoundedSurface =
+                    yGradient(192, 256, 1.0, 0.0) * (0.9375.df + id("nectere/surface_3d_noise").df) - 0.9375.df
+                val surface = 2.5.df + yGradient(160, 224, 0.65, 1.0) * (upperBoundedSurface - 2.5.df)
+
+                finalDensity =
+                    ((yGradient(160, 224, 1.0, 0.0) * caves + yGradient(160, 224, 0.0, 1.0) * surface).blendDensity()
+                        .interpolated() * 0.64.df).squeeze()
             }
 
             surfaceRule(
@@ -62,8 +61,8 @@ class NoiseSettingsGen(output: FabricDataOutput) : NoiseSettingsProvider(output)
                     ),
                     condition(
                         biome(
-                            RegistryKey.of(RegistryKeys.BIOME, id("thinking_forest")),
-                            RegistryKey.of(RegistryKeys.BIOME, id("meditating_fields"))
+                            id("thinking_forest").asBiomeKey(),
+                            id("meditating_fields").asBiomeKey()
                         ),
                         condition(
                             water(1, 0),
