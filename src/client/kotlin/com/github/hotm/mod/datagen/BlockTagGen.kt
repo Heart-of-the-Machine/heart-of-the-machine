732c13b08efe4a3e0e6f188f333c6a71ecf2503e
Kneelawk <kneelawk@gmail.com>
2023-05-08 16:31:54 -0700

    Fix caves

--- src/client/kotlin/com/github/hotm/mod/datagen/BlockTagGen.kt
+++ src/client/kotlin/com/github/hotm/mod/datagen/BlockTagGen.kt
@@ -1,6 +1,7 @@
 package com.github.hotm.mod.datagen
 
 import java.util.concurrent.CompletableFuture
+import com.github.hotm.mod.Constants.id
 import com.github.hotm.mod.block.HotMBlocks.PLASSEIN_THINKING_SCRAP
 import com.github.hotm.mod.block.HotMBlocks.PLASSEIN_THINKING_SCRAP_LEYLINE
 import com.github.hotm.mod.block.HotMBlocks.RUSTED_THINKING_SCRAP
@@ -12,13 +13,16 @@ import com.github.hotm.mod.block.HotMBlocks.THINKING_STONE_LEYLINE
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider.BlockTagProvider
 import net.minecraft.registry.HolderLookup
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.tag.BlockTags
+import net.minecraft.registry.tag.TagKey
 
 class BlockTagGen(
     output: FabricDataOutput, registriesFuture: CompletableFuture<HolderLookup.Provider>
 ) : BlockTagProvider(output, registriesFuture) {
-    override fun configure(arg: HolderLookup.Provider) {
-        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE).add(
+    companion object {
+        private val BASE_BLOCKS = arrayOf(
             THINKING_STONE,
             THINKING_SCRAP,
             RUSTED_THINKING_SCRAP,
@@ -29,15 +33,16 @@ class BlockTagGen(
             PLASSEIN_THINKING_SCRAP_LEYLINE
         )
 
-        getOrCreateTagBuilder(BlockTags.NEEDS_STONE_TOOL).add(
-            THINKING_STONE,
-            THINKING_SCRAP,
-            RUSTED_THINKING_SCRAP,
-            PLASSEIN_THINKING_SCRAP,
-            THINKING_STONE_LEYLINE,
-            THINKING_SCRAP_LEYLINE,
-            RUSTED_THINKING_SCRAP_LEYLINE,
-            PLASSEIN_THINKING_SCRAP_LEYLINE
+        private val BASE_TAGS = arrayOf(
+            BlockTags.PICKAXE_MINEABLE,
+            BlockTags.NEEDS_STONE_TOOL,
+            TagKey.of(RegistryKeys.BLOCK, id("nectere_carver_replaceables"))
         )
+    }
+
+    override fun configure(arg: HolderLookup.Provider) {
+        for (tag in BASE_TAGS) {
+            getOrCreateTagBuilder(tag).add(*BASE_BLOCKS)
+        }
     }
 }
