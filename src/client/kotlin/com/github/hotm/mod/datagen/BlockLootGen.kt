ef0acbe34fa4c88b355c2d7dc13da7e57a9c36e0
Kneelawk <kneelawk@gmail.com>
2023-06-03 09:09:07 -0700

    Update Minecraft to 1.20-rc1

--- src/client/kotlin/com/github/hotm/mod/datagen/BlockLootGen.kt
+++ src/client/kotlin/com/github/hotm/mod/datagen/BlockLootGen.kt
@@ -1,6 +1,5 @@
 package com.github.hotm.mod.datagen
 
-import java.util.function.BiConsumer
 import com.github.hotm.mod.block.HotMBlocks.PLASSEIN_THINKING_SCRAP
 import com.github.hotm.mod.block.HotMBlocks.PLASSEIN_THINKING_SCRAP_LEYLINE
 import com.github.hotm.mod.block.HotMBlocks.RUSTED_THINKING_SCRAP
@@ -11,8 +10,6 @@ import com.github.hotm.mod.block.HotMBlocks.THINKING_STONE
 import com.github.hotm.mod.block.HotMBlocks.THINKING_STONE_LEYLINE
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider
-import net.minecraft.loot.LootTable
-import net.minecraft.util.Identifier
 
 class BlockLootGen(dataOutput: FabricDataOutput) : FabricBlockLootTableProvider(dataOutput) {
     override fun generate() {
@@ -24,9 +21,5 @@ class BlockLootGen(dataOutput: FabricDataOutput) : FabricBlockLootTableProvider(
         addDrop(THINKING_SCRAP_LEYLINE)
         addDrop(RUSTED_THINKING_SCRAP_LEYLINE)
         addDrop(PLASSEIN_THINKING_SCRAP_LEYLINE)
-    }
-
-    override fun accept(t: BiConsumer<Identifier, LootTable.Builder>?) {
-        super.generate(t)
     }
 }
